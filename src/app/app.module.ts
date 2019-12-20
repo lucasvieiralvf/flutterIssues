6ae5558f69daa71d4050250e17a2393a294dfa6d
Lucas Vieira Fontes <lucasvieiralvf@outlook.com>
2019-12-20 17:07:55 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstcomponentComponent } from './firstcomponent/firstcomponent.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { FirstComponent } from './first/first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstcomponentComponent,
+    FirstComponentComponent,
+    FirstComponent
   ],
   imports: [
     BrowserModule,
